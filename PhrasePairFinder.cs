4d8c7481c3f61424c53256443c6350be2beeef6a
Cristian Mazilu <mazilu6@gmail.com>
2023-12-10 12:06:49 +0200

    Converted PhrasePairFinder constructor with Stream parameters.

--- PhrasePairFinder.cs
+++ PhrasePairFinder.cs
@@ -8,18 +8,22 @@ namespace KeyPhraseMatcher
 {
     public class PhrasePairFinder
     {
-        public PhrasePairFinder(TextReader searchesReader, TextReader titlesReader)
+        public PhrasePairFinder(Stream searches, Stream titles)
         {
             Words = new SortedSet<string>();
 
-            foreach (var word in Extensions.GetWords(titlesReader))
+            using (var searchesReader = new StreamReader(searches))
+            using (var titlesReader = new StreamReader(titles))
             {
-                Words.Add(word);
-            }
+                foreach (var word in Extensions.GetWords(titlesReader))
+                {
+                    Words.Add(word);
+                }
 
-            foreach (var word in Extensions.GetWords(searchesReader))
-            {
-                Words.Add(word);
+                foreach (var word in Extensions.GetWords(searchesReader))
+                {
+                    Words.Add(word);
+                }
             }
         }
 
