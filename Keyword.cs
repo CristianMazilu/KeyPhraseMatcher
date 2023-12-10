590a43f44be5af65a8e3891b9b0585a85f6efbc1
Cristian Mazilu <mazilu6@gmail.com>
2023-12-10 18:42:49 +0200

    Tabula Rasa

---
+++ Keyword.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyPhraseMatcher
+{
+    public class Keyword
+    {
+        public Keyword(string word, phrasesCount)
+        {
+            Word = word;
+        }
+
+        public string Word { get; }
+        public bool[] PhraseMemebership 
+    }
+}
